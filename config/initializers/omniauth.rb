2de36b7c4e14f95119c301e1d43dfad8baca1408
chrismo <chrismo@clabs.org>
2020-03-07 09:35:47 -0600

    Remove default dev OAuth and replace with dev env.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,13 +1,7 @@
 # frozen_string_literal: true
 
-google_client_id = ENV.fetch(
-  "GOOGLE_CLIENT_ID",
-  "28967537447-4r0f1f1gr1gi0mv1g4h49okobschhljd.apps.googleusercontent.com"
-)
-google_client_secret = ENV.fetch(
-  "GOOGLE_CLIENT_SECRET",
-  "vxXBiUKW3mNWsDjWBUXXfZN_"
-)
+google_client_id = ENV["GOOGLE_CLIENT_ID"] || ENV["STANDUP_DEV_GOOGLE_CLIENT_ID"]
+google_client_secret = ENV["GOOGLE_CLIENT_SECRET"] || ENV["STANDUP_DEV_GOOGLE_CLIENT_SECRET"]
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, google_client_id, google_client_secret,
