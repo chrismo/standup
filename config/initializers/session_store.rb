8473564aceb58a45491d3db45404d7999bc5f5b1
chrismo <chrismo@clabs.org>
2020-03-08 11:27:30 -0500

    Rubocop fix-up session_store.rb

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
-Rails.application.config.session_store :cookie_store, {
-  key: "_p_session_id",
-  expire_after: 10.years
-}
+
+Rails.application.config.session_store :cookie_store,
+                                       key: "_p_session_id",
+                                       expire_after: 10.years
